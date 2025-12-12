e1be88e1580df73da4711026d1272f5aa52cf472
damzymichael <105460672+damzymichael@users.noreply.github.com>
2025-12-12 21:14:48 +0100

    Send credit for skills

--- src/controllers/trade.controller.ts
+++ src/controllers/trade.controller.ts
@@ -1,2 +1,12 @@
 import { Request } from "express"
 import { Controller } from "../lib/requestHandler.config"
+
+export default Controller({
+  async getSuggestions(req, res) {
+    const userId = req.user.id
+
+    // const 
+  },
+
+  async getMatches() {},
+})
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -32,7 +32,7 @@ export default Controller({
   async getSkills(req, res) {
     const skills = await prisma.skill.findMany({
       where: { userId: req.user.id },
-      select: { id: true, name: true, level: true },
+      select: { id: true, name: true, level: true, credit: true },
     })
 
     return res.status(200).send(skills)
